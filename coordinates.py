9a37e3c26a9b84a6f0517222e383578dba5bc03e
Bournvita1998 <mohit.kuri@research.iiit.ac.in>
2017-10-03 04:54:50 +0530

    Initial Commit
    Signed-off-by: shucon <shucon01@gmail.com>

---
+++ coordinates.py
@@ -0,0 +1,12 @@
+import subprocess
+import json
+add = raw_input()
+add.replace(" " , "+")
+command = "curl https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=AIzaSyCRVnX1xe1phL8ZSKd7-dv5nJK5pYBiAE8"
+output = subprocess.check_output(command, shell=True)
+array = json.loads(output)
+print("Latitude:")
+print(array['results'][0]['geometry']['location']['lat'])
+print("Longitude:")
+print(array['results'][0]['geometry']['location']['lng'])
+
